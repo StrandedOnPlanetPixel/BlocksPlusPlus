8297b4f6d2b9e9d63b0e646f54513aa324610a66
AmandaAldrich <amlaldrich@gmail.com>
2019-05-07 09:57:30 -0600

    Added some notes to StickyTrapBlock to follow up on later

--- src/main/java/com/example/blocksplusplus/blocks/StickyTrapBlock.java
+++ src/main/java/com/example/blocksplusplus/blocks/StickyTrapBlock.java
@@ -2,11 +2,26 @@ package com.example.blocksplusplus.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.*;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class StickyTrapBlock extends Block {
 
     public StickyTrapBlock(Material blockMaterialIn, MaterialColor blockMapColorIn) {
         super(Block.Properties.create( blockMaterialIn, blockMapColorIn));
+    }
+
+    //I am aware this is depreciated, but I didn't want to lose this train of thought
+    //I'm borrowing the web code temporarily to goof with
+    public void onEntityCollision(IBlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+
+    }
+
+    public boolean isFullCube(IBlockState state) {
+
+        return false;
     }
 
 }
