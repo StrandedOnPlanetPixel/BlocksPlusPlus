cc82731d079a59c0ebf09a4fa303fa0b2497814b
AmandaAldrich <amlaldrich@gmail.com>
2019-05-08 16:12:19 -0600

    Added some notes to StickyTrapBlock to follow up on later

--- src/main/java/com/example/blocksplusplus/blocks/StickyTrapBlock.java
+++ src/main/java/com/example/blocksplusplus/blocks/StickyTrapBlock.java
@@ -4,8 +4,11 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.*;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
 
 public class StickyTrapBlock extends Block {
 
@@ -15,13 +18,26 @@ public class StickyTrapBlock extends Block {
 
     //I am aware this is depreciated, but I didn't want to lose this train of thought
     //I'm borrowing the web code temporarily to goof with
-    public void onEntityCollision(IBlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+    @Override
+    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+        //public void onEntityCollision(IBlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+
+        entityIn.motionX = 0.4D;
+        entityIn.motionZ = 0.4D;
 
     }
 
+    @Override
     public boolean isFullCube(IBlockState state) {
 
         return false;
     }
+
+
+
+
+
+
+
 
 }
