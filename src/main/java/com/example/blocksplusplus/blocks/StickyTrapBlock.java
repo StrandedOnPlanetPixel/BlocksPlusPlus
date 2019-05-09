fd459ccc153b6f3b630b9a57286a6354834d13d9
AmandaAldrich <amlaldrich@gmail.com>
2019-05-09 10:58:14 -0600

    The sticky trap has carpet like qualities now!

--- src/main/java/com/example/blocksplusplus/blocks/StickyTrapBlock.java
+++ src/main/java/com/example/blocksplusplus/blocks/StickyTrapBlock.java
@@ -7,10 +7,16 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReaderBase;
 import net.minecraft.world.World;
 
 
 public class StickyTrapBlock extends Block {
+
+    protected static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D);
+
 
     public StickyTrapBlock(Material blockMaterialIn, MaterialColor blockMapColorIn) {
         super(Block.Properties.create( blockMaterialIn, blockMapColorIn));
@@ -19,11 +25,11 @@ public class StickyTrapBlock extends Block {
     //I am aware this is depreciated, but I didn't want to lose this train of thought
     //I'm borrowing the web code temporarily to goof with
     @Override
-    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
-        //public void onEntityCollision(IBlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+    //public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+    public void onEntityCollision(IBlockState state, World worldIn, BlockPos pos, Entity entityIn) {
 
-        entityIn.motionX = 0.4D;
-        entityIn.motionZ = 0.4D;
+        entityIn.motionX *= 0.4D;
+        entityIn.motionZ *= 0.4D;
 
     }
 
@@ -31,6 +37,16 @@ public class StickyTrapBlock extends Block {
     public boolean isFullCube(IBlockState state) {
 
         return false;
+    }
+
+    public VoxelShape getShape(IBlockState state, IBlockReader worldIn, BlockPos pos) {
+
+        return SHAPE;
+
+    }
+
+    public boolean isValidPosition(IBlockState state, IWorldReaderBase worldIn, BlockPos pos) {
+        return !worldIn.isAirBlock(pos.down());
     }
 
 
