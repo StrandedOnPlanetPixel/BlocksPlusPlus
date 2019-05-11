631876f6b4caf81758ab889ecbab6d305ab75b8c
AmandaAldrich <amlaldrich@gmail.com>
2019-05-10 21:04:35 -0600

    There was no appreciable improvement to the block.... it is a highly ineffective flypaper alternative currently.

--- src/main/java/com/example/blocksplusplus/blocks/StickyTrapBlock.java
+++ src/main/java/com/example/blocksplusplus/blocks/StickyTrapBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.*;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.MoverType;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -12,26 +13,35 @@ import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReaderBase;
 import net.minecraft.world.World;
 
+import static net.minecraft.entity.MoverType.SELF;
+
 
 public class StickyTrapBlock extends Block {
 
-    protected static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D);
+    protected static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 0.5D, 16.0D);
 
 
     public StickyTrapBlock(Material blockMaterialIn, MaterialColor blockMapColorIn) {
         super(Block.Properties.create( blockMaterialIn, blockMapColorIn));
     }
 
-    //I am aware this is depreciated, but I didn't want to lose this train of thought
-    //I'm borrowing the web code temporarily to goof with
+    //This method haunts my dreams
     @Override
     //public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
     public void onEntityCollision(IBlockState state, World worldIn, BlockPos pos, Entity entityIn) {
 
-        entityIn.motionX *= 0.4D;
-        entityIn.motionZ *= 0.4D;
+        entityIn.posX = pos.getX();
+        entityIn.posY = pos.getY();
+        entityIn.posZ = pos.getZ();
+
+        entityIn.motionX = 0;
+        entityIn.motionZ = 0;
+        entityIn.motionY = 0;
+
+        entityIn.setVelocity(0,0,0);
 
     }
+
 
     @Override
     public boolean isFullCube(IBlockState state) {
